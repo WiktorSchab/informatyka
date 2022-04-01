982b97db7c055632c39d26e37c7e2055768dbb83
WiktorSchab <wiktormateuszschab@gmail.com>
2022-04-01 12:41:07 +0200

    Liczby pierwsze zadanie

--- Liczby Pierwsze/Liczby Pierwsze.cpp
+++ Liczby Pierwsze/Liczby Pierwsze.cpp
@@ -1,21 +1,35 @@
-﻿// Liczby Pierwsze.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
-//
+﻿#include<iostream>
+using namespace std;
 
-#include <iostream>
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	int i, j, liczenie = 1, x = 0, ilosc = 0;
 
+	cout << "Podaj ilość liczb pierwszych jaka chcesz poznać ";
+	cin >> ilosc;
+
+	cout << "Liczby pierwsze to ";
+
+	for (i = 3; i > 0; ++i)
+	{
+		for (j = 2; j <= i / 2; ++j)
+		{
+			if (i % j == 0) {
+				x = 1;
+				break;
+			}
+		}
+		if (x == 0)
+		{
+			cout << i;
+			cout << " ";
+			liczenie = liczenie + 1;
+		}
+		x = 0;
+		if (liczenie == ilosc + 1)
+			break;
+	}
+	return 0;
 }
-
-// Uruchomienie programu: Ctrl + F5 lub menu Debugowanie > Uruchom bez debugowania
-// Debugowanie programu: F5 lub menu Debugowanie > Rozpocznij debugowanie
-
-// Porady dotyczące rozpoczynania pracy:
-//   1. Użyj okna Eksploratora rozwiązań, aby dodać pliki i zarządzać nimi
-//   2. Użyj okna programu Team Explorer, aby nawiązać połączenie z kontrolą źródła
-//   3. Użyj okna Dane wyjściowe, aby sprawdzić dane wyjściowe kompilacji i inne komunikaty
-//   4. Użyj okna Lista błędów, aby zobaczyć błędy
-//   5. Wybierz pozycję Projekt > Dodaj nowy element, aby utworzyć nowe pliki kodu, lub wybierz pozycję Projekt > Dodaj istniejący element, aby dodać istniejące pliku kodu do projektu
-//   6. Aby w przyszłości ponownie otworzyć ten projekt, przejdź do pozycji Plik > Otwórz > Projekt i wybierz plik sln
